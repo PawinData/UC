8ba35fc76fc30efd4583169575dbcd8bf2ba3466
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-04-11 00:25:49 +0200

    create data matrix of confirmed cases

---
+++ DataMatrix.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv('https://query.data.world/s/7ifvvpyy23sa2fciuvcygehakxlnnv')
+
+# only look at counties in California
+
+
+
+# only look at 16 counties of Los Angeles Metropolitan Area
