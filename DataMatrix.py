9bf747c727286ec4ba0debbcfbfeb901402846b9
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-04-12 03:29:01 +0200

    Delete DataMatrix.py

--- DataMatrix.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('https://query.data.world/s/7ifvvpyy23sa2fciuvcygehakxlnnv')
-
-# only look at counties in California
-
-
-
-# only look at 16 counties of Los Angeles Metropolitan Area
