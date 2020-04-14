e3a71a5eb63734ece27930956d597b0b28d3d556
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-04-14 22:38:05 +0200

    Intra-/Inter-Region Plotting

---
+++ plot_Tempo_Spatio.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from functions import str_to_day, day_to_str
+
+# plot the Temporal and Spatial structures of a DataFrame
+# each row is a time series for a discrete location
+# Spatial differences are measured against the Distance Matrix
+
+def plot_Tempo_Spatio(DF, DistanceMatrix):
+
+    START = dict()  # the day when the first case was confirmed in each county
+    for cnty in DF.index:
+        for date in DF.columns:
+            if not DF[date][cnty]==0:
+                START[cnty] = str_to_day(date)
+                break
+    
+    H = dict()   # a histogram of pairwise delta t
+    S = dict()   # sum of pairwise differences over all delta t
+    for cnty in DF.index:               # loop through every county
+    for t1 in range(START[cnty],1+str_to_day(t[-1])): 
+        day1 = day_to_str(t1)            # for every two days
+        for t2 in range(START[cnty],1+str_to_day(t[-1])):# compute the |diff| in number of new cases
+            delta = abs(t1 - t2)
+            if delta==0:
+                continue
+            day2 = day_to_str(t2)
+            H[delta] = H.get(delta,0) + 1    # count records (to compute average)
+            # add up total |diff|
+            S[delta] = S.get(delta,0) + abs(DF[day1][cnty] - DF[day2][cnty])
+            
+    LST = list(H.keys())
+    LST.sort()
+            
+            
+    h = dict() # a histogram of pairwise distances
+    s = dict() # sum of pairwise differences over all distances
+    for date in DF.columns:
+        for A in DF.index:
+            for B in DF.index:
+            if A==B:
+                continue
+            d = DistanceMatrix[A][B]
+            h[d] = h.get(d1,0) + 1
+            s[d] = s.get(d,0) + abs(DF[date][A] - DF[date][B])
+     
+    lst = list(H_1.keys())
+    lst.sort()
+    
+    fig, axs = plt.subplots(2, figsize=(10,20))
+    fig.suptitle("Tempo-Spatial Correlation", fontsize=25)
+
+    # the subplot for Intra-Region Temporal Correlation
+    axs[0].plot(LST, [int(S[delta])/int(H[delta]) for delta in LST])
+    axs[0].set_title("Intra-Region Temporal Correlation", fontsize=20)
+    axs[0].set_xlabel("$\Delta$ t", fontsize=20)
+    axs[0].set_ylabel("$\Delta$ daily new cases", fontsize=20)
+
+    # the subplot for Inter-Region Spatial Correlation
+    axs[1].plot(lst, [int(s[delta])/int(h[delta]) for delta in lst], 'g')
+    axs[1].set_title("Inter-Region Spatial Correlation", fontsize=20)
+    axs[1].set_xlabel("$\Delta$ d", fontsize=20)
+    axs[1].set_ylabel("$\Delta$ daily new cases", fontsize=20)
+
+    plt.savefig("Tempo_Spatio.eps")
+    plt.savefig("Tempo_Spatio.png")
