d6216480cdfeba06be5069b9c2648cb96f1e4982
Unknown <y.creak@hotmail.com>
2020-05-19 11:53:38 +0200

    Weather Data Complete (-SF)
    San Francisco has a bug, will add that later (hopefully)

---
+++ Data/Foursquare/categorySelector.py
@@ -0,0 +1,16 @@
+# Input CSV, selects counties from list, output data of those counties
+import csv
+import pandas as pd
+
+df = pd.read_csv('visitDataCounties.csv', sep=',')
+
+list = ("Food","Food")
+
+item = 'Nightlife Spots'
+
+# df = df[df['categoryName'].isin(list)]
+df = df[df['categoryName'] == item ]
+
+print(df)
+
+df.to_csv('./temp.csv', index = False, header=True)
--- Data/WeatherData/GoogleEngine.js
+++ Data/WeatherData/GoogleEngine.js
@@ -1,24 +1,16 @@
-// Define a FeatureCollection: regions of the American West.
+// https://developers.google.com/earth-engine/datasets/catalog/NASA_NLDAS_FORA0125_H002#bands
+
 // Define a FeatureCollection: regions of the American West.
 var regions = ee.FeatureCollection([
-  ee.Feature(    // San Francisco.
-    ee.Geometry.Point(-123.032229, 37.727239), {label: 'San Francisco'}),
-  ee.Feature(  // Tahoe National Forest.
-    ee.Geometry.Point(-122.325995, 38.507351), {label: 'Napa'}),
-  ee.Feature(  // Black Rock Desert.
-    ee.Geometry.Point(-121.690622, 37.220777), {label: 'Santa Clara'}),
-  ee.Feature(    // San Francisco.
-    ee.Geometry.Point(-122.945194, 38.532574), {label: 'Sonoma'}),
-  ee.Feature(  // Tahoe National Forest.
-    ee.Geometry.Point(-121.913304, 37.648081), {label: 'Alameda'}),
-  ee.Feature(  // Black Rock Desert.
-    ee.Geometry.Point(-122.371542, 37.414664), {label: 'San Mateo'}),
-  ee.Feature(    // San Francisco.
-    ee.Geometry.Point(-121.939594, 38.267226), {label: 'Solano'}),
-  ee.Feature(  // Tahoe National Forest.
-    ee.Geometry.Point(-121.951543, 37.919479), {label: 'Contra Costa'}),
-  ee.Feature(  // Black Rock Desert.
-    ee.Geometry.Point(-122.745974, 38.051817), {label: 'Marin'})    
+  ee.Feature(ee.Geometry.Point(-123.032229, 37.727239), {label: 'San Francisco'}),
+  ee.Feature(ee.Geometry.Point(-122.325995, 38.507351), {label: 'Napa'}),
+  ee.Feature(ee.Geometry.Point(-121.690622, 37.220777), {label: 'Santa Clara'}),
+  ee.Feature(ee.Geometry.Point(-122.945194, 38.532574), {label: 'Sonoma'}),
+  ee.Feature(ee.Geometry.Point(-121.913304, 37.648081), {label: 'Alameda'}),
+  ee.Feature(ee.Geometry.Point(-122.371542, 37.414664), {label: 'San Mateo'}),
+  ee.Feature(ee.Geometry.Point(-121.939594, 38.267226), {label: 'Solano'}),
+  ee.Feature(ee.Geometry.Point(-121.951543, 37.919479), {label: 'Contra Costa'}),
+  ee.Feature(ee.Geometry.Point(-122.745974, 38.051817), {label: 'Marin'})    
 ]);
 
 var select = 'tmax'
@@ -49,4 +41,4 @@ var tempTimeSeries = ui.Chart.image.seriesByRegion(
 }});
 
 // Display.
-print(tempTimeSeries);
+print(tempTimeSeries)
